0fdab1ca0c39fb75b322124c596a1395cef270b3
xPerSki <97462549+xPerSki@users.noreply.github.com>
2022-05-10 14:07:45 +0200

    Add files via upload

---
+++ AI/SETTINGS.py
@@ -0,0 +1,22 @@
+# HAARcascades location
+face_data = 'C:/Users/Admin/Desktop/AI/haarcascades/frontal_face.xml'
+eye_data = 'C:/Users/Admin/Desktop/AI/haarcascades/eye.xml'
+smile_data = 'C:/Users/Admin/Desktop/AI/haarcascades/smile.xml'
+upperbody_data = 'C:/Users/Admin/Desktop/AI/haarcascades/upperbody.xml'
+
+# HAARcascades confidence
+face_scaleFactor = 1.23
+face_minNeighbors = 5
+face_minSize = (50, 50)
+
+smile_scaleFactor = 1.9
+smile_minNeighbors = 20
+
+eye_scaleFactor = 1.7
+eye_minNeighbors = 8
+
+# Mirroring video
+mirror = True
+
+# STOP key
+stop_key = 27   # [Esc]
